3183faff011439a64faf91568a31f3dc14ac47ad
bhuvishi <bhuvishi.bansal@gmail.com>
2022-12-14 17:00:15 +0530

    first commit

---
+++ fill_mask/fill_mask.py
@@ -0,0 +1,46 @@
+from transformers import pipeline
+import wiki_scraper
+import gender_bias
+import numpy as np
+
+
+def get_results_for_url(url, unmasker):
+    text_chunks = wiki_scraper.get_text_chunks_from_url(url)
+    for text_chunk in text_chunks:
+        masked_str_and_targets = gender_bias.get_masked_str_and_targets(
+            text_chunk)
+        for masked_str, targets in masked_str_and_targets:
+            results = unmasker(masked_str, targets=targets)
+            assert (len(results) == 2)
+            yield results
+
+
+def collect_statistics_from_results(results, output_score_statistics):
+    assert (len(results) == 2)
+    output_score_statistics.setdefault('score_ratios', []).append(
+        results[0]['score'] / results[1]['score'])
+
+
+def get_statistics_for_urls(urls, unmasker):
+    score_statistics = {}
+    for url in urls:
+        results = get_results_for_url(url, unmasker)
+        for result in results:
+            collect_statistics_from_results(result, score_statistics)
+    return score_statistics
+
+
+urls = [
+    'https://en.m.wikipedia.org/wiki/Engineer',
+    'https://en.wikipedia.org/wiki/Chief_executive_officer',
+    'https://en.wikipedia.org/wiki/Police_officer',
+    'https://en.wikipedia.org/wiki/Technician',
+    'https://en.wikipedia.org/wiki/Aircraft_pilot',
+    'https://en.wikipedia.org/wiki/Author',
+    'https://en.wikipedia.org/wiki/Businessperson']
+unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
+score_statistics = get_statistics_for_urls(urls, unmasker)
+print('count={0}, mean={1}, median={2}'.format(
+    len(score_statistics['score_ratios']),
+    np.mean(score_statistics['score_ratios']),
+    np.median(score_statistics['score_ratios'])))
---
+++ fill_mask/gender_bias.py
@@ -0,0 +1,20 @@
+import re
+
+CATCH_WORD_TO_PRONOUNS = {
+    'he/she': ['he', 'she'],
+    'him/her': ['him', 'her'],
+    'his/her': ['his', 'her'],
+    'they': ['he', 'she'],
+    'them': ['him', 'her']
+}
+
+
+def get_masked_str_and_targets(text):
+    text_words = text.split(' ')
+    masked_str_and_targets = []
+    for i in range(len(text_words)):
+        pronouns = CATCH_WORD_TO_PRONOUNS.get(text_words[i], None)
+        if pronouns:
+            masked_str = ' '.join(
+                text_words[:i] + ['[MASK]'] + text_words[i + 1:])
+            yield (masked_str, pronouns)
